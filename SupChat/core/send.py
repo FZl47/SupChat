5dc1ccc1ffc6fee7f33494344402f62ec154107e
FZl47 <wwwfazel88@gmail.com>
2022-09-20 03:03:57 +0430

    Rename class TypeMethods to Response and some little edit

--- SupChat/core/send.py
+++ SupChat/core/send.py
@@ -6,56 +6,55 @@ from SupChat.models import Message, TextMessage
 
 # TYPE_RESPONSE = ['TEXT_MESSAGE']
 
-class TypeMethods:
+class Response:
+    RESPONSES = {
+        # You should define name request and method for response
+        # REQUESTS TYPE and REQUEST HANDLER
+        'SEND_TEXT_MESSAGE': 'send_text_message',
+        'DELETE_MESSAGE': 'delete_message'
+    }
 
-    def _send_to_group(self,data):
+    # Base Method
+    def send_to_group(self, type_response, data):
         async_to_sync(self.channel_layer.group_send)(
             self.chat.get_group_name(),
-            data
-        )
-
-    def send_text_message(self,text_message):
-        if text_message:
-            group_name = self.chat.get_group_name()
-            message_object = TextMessage.objects.create(chat=self.chat, sender=self.type_user,
-                                                        text=text_message)
-            type_func = 'type_send_data'
-            context = {
-                'type': type_func,
+            {
+                'type': 'type_send_data',
                 'data': {
-                    'TYPE_RESPONSE': 'TEXT_MESSAGE',
-                    'message': serializers.Serializer_message(message_object)
+                    'TYPE_RESPONSE': type_response,
+                    **data
                 }
             }
-            self._send_to_group(context)
+        )
 
-    # def type_send_text_message(self, event):
-    #     data = json.dumps(event['data'])
-    #     self.send(text_data=data)
+    # Base Method
+    def type_send_data(self, event):
+        data = json.dumps(event['data'])
+        self.send(text_data=data)
 
-    
-    def delete_message(self,message_id):
+
+
+    # Handlers
+    def send_text_message(self, data_request):
+        text_message = data_request.get('message')
+        if text_message:
+            message_object = TextMessage.objects.create(chat=self.chat, sender=self.type_user,
+                                                        text=text_message)
+            self.send_to_group('TEXT_MESSAGE', {
+                'message': serializers.Serializer_message(message_object)
+            })
+
+
+    def delete_message(self, data_request):
+        message_id = data_request.get('id')
         if message_id:
-            group_name = self.chat.get_group_name()
-            message_object = Message.objects.filter(id=message_id,chat=self.chat, sender=self.type_user).select_subclasses().first()
+            message_object = Message.objects.filter(id=message_id, chat=self.chat,
+                                                    sender=self.type_user).select_subclasses().first()
             if message_object:
                 message_object.deleted = True
                 message_object.save()
-                type_func = 'type_send_data'
-                context = {
-                    'type': type_func,
-                    'data': {
-                        'TYPE_RESPONSE': 'DELETE_MESSAGE',
-                        'message': serializers.Serializer_message(message_object)
-                    }
-                }
-                self._send_to_group(context)
-
-    # def type_delete_message(self,event):
-    #     data = json.dumps(event['data'])
-    #     self.send(text_data=data)
+                self.send_to_group('DELETE_MESSAGE', {
+                    'message': serializers.Serializer_message(message_object)
+                })
 
 
-    def type_send_data(self,event):
-        data = json.dumps(event['data'])
-        self.send(text_data=data)

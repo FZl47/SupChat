d96e022f39009bec05f06d76807f97b02f951ddb
FZl47 <wwwfazel88@gmail.com>
2022-09-21 05:15:05 +0430

    Add is_typing in chat

--- assets/supchat/js/request_response.js
+++ assets/supchat/js/request_response.js
@@ -3,7 +3,7 @@ class ResponseSupChat {
 
     static get(type) {
         return ResponseSupChat.LIST_RESPONSE_SUPCHAT.find(function (obj, index, arr) {
-                if (obj.TYPE_RESPONSE ==  type) {
+                if (obj.TYPE_RESPONSE == type) {
                     return obj
                 }
             }
@@ -12,14 +12,15 @@ class ResponseSupChat {
 
     constructor(TYPE_RESPONSE, FUNC) {
         ResponseSupChat.LIST_RESPONSE_SUPCHAT.push(this)
-        this.TYPE_RESPONSE =  TYPE_RESPONSE
+        this.TYPE_RESPONSE = TYPE_RESPONSE
         this.FUNC = FUNC
     }
 
     run(data) {
+        // Normilize Data
+        data = JSON.parse(JSON.stringify(data))
         this.FUNC(data)
     }
-
 }
 
 
@@ -33,32 +34,37 @@ new ResponseSupChat('TEXT_MESSAGE', function (data) {
 
 // response delete message
 new ResponseSupChat('DELETE_MESSAGE', function (data) {
-    let message_obj = MessageSupChat.get(data.message.id)   
-    if (message_obj){
+    let message_obj = MessageSupChat.get(data.message.id)
+    if (message_obj) {
         message_obj.delete_element()
     }
 })
 
+
+// response is typing
+new ResponseSupChat('IS_TYPING', function (data) {
+    let state_is_typing = data.state_is_typing
+    SUP_CHAT.effect_is_typing(state_is_typing)
+})
 
 
 // --- REQUESTS ---
 
 class _RequestBaseSupChat {
     constructor(TYPE_REQUEST) {
-        this.TYPE_REQUEST =  TYPE_REQUEST
+        this.TYPE_REQUEST = TYPE_REQUEST
     }
 
-    send_to_socket(data){
-        if (SUP_CHAT.SOCKET.readyState == 1){
+    send_to_socket(data) {
+        if (SUP_CHAT.SOCKET.readyState == 1) {
             data['TYPE_REQUEST'] = this.TYPE_REQUEST
             SUP_CHAT.SOCKET.send(JSON.stringify(data))
         }
     }
-
 }
 
 
-class RequestSendTextMessageSupChat extends _RequestBaseSupChat{
+class RequestSendTextMessageSupChat extends _RequestBaseSupChat {
 
     constructor() {
         super('SEND_TEXT_MESSAGE');
@@ -72,8 +78,7 @@ class RequestSendTextMessageSupChat extends _RequestBaseSupChat{
     }
 }
 
-class RequestSendAudioMessageSupChat extends _RequestBaseSupChat{
-
+class RequestSendAudioMessageSupChat extends _RequestBaseSupChat {
     constructor() {
         super('SEND_AUDIO_MESSAGE');
     }
@@ -87,8 +92,7 @@ class RequestSendAudioMessageSupChat extends _RequestBaseSupChat{
 }
 
 
-class RequestDeleteMessageSupChat extends _RequestBaseSupChat{
-
+class RequestDeleteMessageSupChat extends _RequestBaseSupChat {
     constructor() {
         super('DELETE_MESSAGE');
     }
@@ -101,8 +105,22 @@ class RequestDeleteMessageSupChat extends _RequestBaseSupChat{
     }
 }
 
+class RequestIsTypingSupChat extends _RequestBaseSupChat {
+    constructor() {
+        super('IS_TYPING');
+    }
+
+    send(state_is_typing) {
+        let data = {
+            'state_is_typing': state_is_typing
+        }
+        this.send_to_socket(data)
+    }
+}
+
 const RequestSupChat = {
     'text_message': new RequestSendTextMessageSupChat(),
     'audio_message': new RequestSendAudioMessageSupChat(),
     'delete_message': new RequestDeleteMessageSupChat(),
+    'is_typing': new RequestIsTypingSupChat(),
 }

169df0267a31c869d64a03808979d621d9a0d53e
FZl47 <wwwfazel88@gmail.com>
2022-09-21 05:21:17 +0430

    Add is_typing response

--- SupChat/core/send.py
+++ SupChat/core/send.py
@@ -11,13 +11,14 @@ class Response:
         # You should define name request and method for response
         # REQUESTS TYPE and REQUEST HANDLER
         'SEND_TEXT_MESSAGE': 'send_text_message',
-        'DELETE_MESSAGE': 'delete_message'
+        'DELETE_MESSAGE': 'delete_message',
+        'IS_TYPING': 'is_typing'
     }
 
     # Base Method
-    def send_to_group(self, type_response, data):
+    def send_to_group(self, type_response, data, group_name=None):
         async_to_sync(self.channel_layer.group_send)(
-            self.chat.get_group_name(),
+            group_name or self.chat.get_group_name(),
             {
                 'type': 'type_send_data',
                 'data': {
@@ -32,8 +33,6 @@ class Response:
         data = json.dumps(event['data'])
         self.send(text_data=data)
 
-
-
     # Handlers
     def send_text_message(self, data_request):
         text_message = data_request.get('message')
@@ -43,7 +42,6 @@ class Response:
             self.send_to_group('TEXT_MESSAGE', {
                 'message': serializers.Serializer_message(message_object)
             })
-
 
     def delete_message(self, data_request):
         message_id = data_request.get('id')
@@ -57,4 +55,10 @@ class Response:
                     'message': serializers.Serializer_message(message_object)
                 })
 
-
+    def is_typing(self, data_request):
+        state_is_typing = data_request.get('state_is_typing')
+        if (state_is_typing == True) or (state_is_typing == False) :
+            group_name_reciver = self.chat.get_group_name_admin() if self.type_user == 'user' else self.chat.get_group_name_user()
+            self.send_to_group('IS_TYPING', {
+                'state_is_typing': state_is_typing
+            }, group_name_reciver)

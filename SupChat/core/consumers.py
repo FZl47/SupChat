7ec5f3776e459083f2c1e605c1e7720eb3d92116
FZl47 <wwwfazel88@gmail.com>
2022-09-21 05:23:34 +0430

    Add class AdminUser for chat admin

--- SupChat/core/consumers.py
+++ SupChat/core/consumers.py
@@ -24,25 +24,43 @@ class SupChat(WebsocketConsumer,send.Response):
             group_name,
             self.channel_name
         )
-
+        
+    def receive(self, text_data=None, bytes_data=None):
+        text_data = json.loads(text_data)
+        type_request = text_data.get('TYPE_REQUEST')
+        handler_response_name = self.RESPONSES.get(type_request,'')
+        handler_response = getattr(self,handler_response_name,None)
+        if handler_response:
+            handler_response(text_data)
 
 
 class ChatUser(SupChat):
     """
         Order of decorators is important
     """
+    type_user = 'user'
 
     @decorators.user_authenticated
-    @decorators.get_chat('user')
+    @decorators.get_chat(type_user)
     def connect(self):
-        self.type_user = 'user'
         self.add_to_group(self.chat.get_group_name())
+        self.add_to_group(self.chat.get_group_name_user())
         self.accept()
 
-    def receive(self, text_data=None, bytes_data=None):
-        text_data = json.loads(text_data)
-        type_request = text_data.get('TYPE_REQUEST')
-        handler_request_name = self.RESPONSES.get(type_request,'')
-        handler_request = getattr(self,handler_request_name,None)
-        if handler_request:
-            handler_request(text_data)
+
+
+class AdminUser(SupChat):
+    """
+        Order of decorators is important
+    """
+    type_user = 'admin'
+
+    @decorators.admin_authenticated
+    @decorators.get_chat(type_user)
+    def connect(self):
+        self.add_to_group(self.chat.get_group_name())
+        self.add_to_group(self.chat.get_group_name_admin())
+        self.accept()
+        
+
+

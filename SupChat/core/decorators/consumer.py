9770083031e01e20e4c727fa32d83b94e900931a
FZl47 <wwwfazel88@gmail.com>
2022-09-21 05:20:53 +0430

    Remove useless comment

--- SupChat/core/decorators/consumer.py
+++ SupChat/core/decorators/consumer.py
@@ -15,9 +15,6 @@ def user_authenticated(func):
 
 
 def admin_authenticated(func):
-    """
-        check user is admin or not - if user is admin so set attribute admin => self.admin = admin
-    """
     def wrapper(self):
         user = self.scope['user']
         admin = Admin.objects.filter(user=user).first()

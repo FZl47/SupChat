c10f8245ed0a615fb3f7a4aecdf0adeff22f29c1
FZl47 <wwwfazel88@gmail.com>
2022-09-22 21:13:05 +0330

    Edit little in node audio message and Add container-type sending voice

--- assets/supchat/js/nodes.js
+++ assets/supchat/js/nodes.js
@@ -138,18 +138,26 @@ function get_node_supchat() {
                     </div> 
                     <div container-type="voice-send-or-cancel">
                         <div>
-                            <p>... ${SUP_CHAT.TRANSLATE.get('صدای ضبط شده')}</p>
-                            <p id="time-voice-recorded-supchat">0.0</p>
-                        </div>
-                        <div>
-                            <button id="btn-voice-cancel-supchat">    
-                                <i class="fal fa-times"></i>
-                            </button>
                             <button id="btn-send-voice-recorded-supchat">    
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <path d="M20.34,9.32l-14-7a3,3,0,0,0-4.08,3.9l2.4,5.37h0a1.06,1.06,0,0,1,0,.82l-2.4,5.37A3,3,0,0,0,5,22a3.14,3.14,0,0,0,1.35-.32l14-7a3,3,0,0,0,0-5.36Zm-.89,3.57-14,7a1,1,0,0,1-1.35-1.3l2.39-5.37A2,2,0,0,0,6.57,13h6.89a1,1,0,0,0,0-2H6.57a2,2,0,0,0-.08-.22L4.1,5.41a1,1,0,0,1,1.35-1.3l14,7a1,1,0,0,1,0,1.78Z"></path>
                                 </svg>
                             </button>
+                            <button id="btn-voice-cancel-supchat">    
+                                <i class="fal fa-times"></i>
+                            </button>
+                        </div>
+                        <div>
+                            <p id="time-voice-recorded-supchat">0.0</p>
+                            <p>... ${SUP_CHAT.TRANSLATE.get('صدای ضبط شده')}</p>
+                        </div>
+                    </div>
+                    <div container-type="voice-sending">
+                        <div>
+                            <div class="loading-circle-supchat-small"></div>
+                        </div>
+                        <div>
+                            <p>${SUP_CHAT.TRANSLATE.get('در حال ارسال')}</p>
                         </div>
                     </div>
                 </div>
@@ -190,11 +198,11 @@ function get_node_footer_message_you(message) {
             <div>
                 <i class="icon-message-edited-supchat fal fa-pen"></i>
                 ${SUP_CHAT.CONFIG.show_seen_message ?
-        `<div class="seen-message-supchat">
+                    `<div class="seen-message-supchat">
                         ${get_node_seen_true()}
                         ${get_node_seen_false()}
                     </div>` : ''
-    }
+                }
             </div>
             </footer>  
     `
@@ -280,7 +288,7 @@ function get_node_audio_message_you(message) {
 
 
     return `
-        <div class="content-message-supchat">
+        <div class="content-message-supchat" edited="false">
             <header>
             </header>
             <main>

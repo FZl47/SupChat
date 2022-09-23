0ea40207c424285362272e6069640afe6e2528ed
FZl47 <wwwfazel88@gmail.com>
2022-09-23 22:03:45 +0330

    Add func get time

--- SupChat/config.py
+++ SupChat/config.py
@@ -1,5 +1,8 @@
 """ Config SupChat """
-
+import datetime
+import pytz
+from django.conf import settings
+from django.utils import timezone
 
 """  Note!  """
 #You must set name settings project in file "asgi.py"
@@ -10,3 +13,9 @@
 from django.contrib.auth.models import User
 USER = User
 
+
+def get_datetime():
+    # tz_str = settings.TIME_ZONE or 'UTC'
+    # tz = pytz.timezone(tz_str)
+    t = timezone.now()
+    return t

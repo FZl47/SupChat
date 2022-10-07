23d22c7c42eae0d33f65cd348358073580f0a2b2
FZl47 <wwwfazel88@gmail.com>
2022-10-07 14:27:59 +0330

    some modify.

--- assets/supchat/js/elements.js
+++ assets/supchat/js/elements.js
@@ -182,11 +182,12 @@ function get_node_supchat() {
                     </div>
                      <div container-type="send-message-main">
                         <div class="container-btns-send-message-voice">
-                            <button id="btn-record-voice-supchat">
+                            ${is_secure() ?
+                            ` <button id="btn-record-voice-supchat">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <path d="M12,15a4,4,0,0,0,4-4V5A4,4,0,0,0,8,5v6A4,4,0,0,0,12,15ZM10,5a2,2,0,0,1,4,0v6a2,2,0,0,1-4,0Zm10,6a1,1,0,0,0-2,0A6,6,0,0,1,6,11a1,1,0,0,0-2,0,8,8,0,0,0,7,7.93V21H9a1,1,0,0,0,0,2h6a1,1,0,0,0,0-2H13V18.93A8,8,0,0,0,20,11Z"></path>
                                 </svg>
-                            </button>
+                            </button>` : ''}
                              <button id="btn-send-message-supchat">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <path d="M20.34,9.32l-14-7a3,3,0,0,0-4.08,3.9l2.4,5.37h0a1.06,1.06,0,0,1,0,.82l-2.4,5.37A3,3,0,0,0,5,22a3.14,3.14,0,0,0,1.35-.32l14-7a3,3,0,0,0,0-5.36Zm-.89,3.57-14,7a1,1,0,0,1-1.35-1.3l2.39-5.37A2,2,0,0,0,6.57,13h6.89a1,1,0,0,0,0-2H6.57a2,2,0,0,0-.08-.22L4.1,5.41a1,1,0,0,1,1.35-1.3l14,7a1,1,0,0,1,0,1.78Z"></path>

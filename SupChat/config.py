e43393c1e8283af4c9540d33c8046ad6ee8773f0
FZl47 <wwwfazel88@gmail.com>
2022-10-14 23:54:29 +0330

    Add var URL_BACKEND_SUPCHAT

--- SupChat/config.py
+++ SupChat/config.py
@@ -3,19 +3,20 @@ import datetime
 import pytz
 from django.conf import settings
 from django.utils import timezone
-
 """  Note!  """
 #You must set name settings project in file "asgi.py"
 
+
+URL_BACKEND_SUPCHAT = 'http://127.0.0.1:8000'
 
 # User Default is Django User
 # if you want change user you must edit "getUser" and "getUserConsumer" function in core.auth.view and core.auth.consumer
 from django.contrib.auth.models import User
 USER = User
 
-
 def get_datetime():
     # tz_str = settings.TIME_ZONE or 'UTC'
     # tz = pytz.timezone(tz_str)
     t = timezone.now()
     return t
+

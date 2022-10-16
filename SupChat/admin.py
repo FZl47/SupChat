71c4ce1f21aab0ff01957246c09db18adbe1b0b0
FZl47 <wwwfazel88@gmail.com>
2022-10-16 22:13:04 +0330

    Add model admin for model ChatGroup | filter by is_active or not

--- SupChat/admin.py
+++ SupChat/admin.py
@@ -9,10 +9,16 @@ admin.site.register(Admin)
 admin.site.register(User)
 admin.site.register(Section)
 admin.site.register(SuggestedMessage)
-admin.site.register(ChatGroup)
+
 admin.site.register(Message)
 admin.site.register(TextMessage)
 admin.site.register(AudioMessage)
 admin.site.register(SystemMessage)
 admin.site.register(SearchHistoryAdmin)
 admin.site.register(BlackList)
+
+@admin.register(ChatGroup)
+class ChatGroupAdmin(admin.ModelAdmin):
+    list_filter = (
+        'is_active',
+    )

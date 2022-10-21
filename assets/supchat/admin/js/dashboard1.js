4bc72101c58ed98d950f9c9ca5ac8382ba21f6c9
FZl47 <wwwfazel88@gmail.com>
2022-10-21 23:52:22 +0330

    remove useless and commented code

--- assets/supchat/admin/js/dashboard1.js
+++ assets/supchat/admin/js/dashboard1.js
@@ -1,262 +1,3 @@
-$(function () {
-    "use strict";
-    // ============================================================== 
-    // Our Visitor
-    // ============================================================== 
-
-    // var chart = c3.generate({
-    //     bindto: '#visitor',
-    //     data: {
-    //         columns: [
-    //             ['Other', 30],
-    //             ['Desktop', 10],
-    //             ['Tablet', 40],
-    //             ['Mobile', 50],
-    //         ],
-    //
-    //         type: 'donut',
-    //         onclick: function (d, i) {
-    //             console.log("onclick", d, i);
-    //         },
-    //         onmouseover: function (d, i) {
-    //             console.log("onmouseover", d, i);
-    //         },
-    //         onmouseout: function (d, i) {
-    //             console.log("onmouseout", d, i);
-    //         }
-    //     },
-    //     donut: {
-    //         label: {
-    //             show: false
-    //         },
-    //         title: "Visits",
-    //         width: 20,
-    //
-    //     },
-    //
-    //     legend: {
-    //         hide: true
-    //         //or hide: 'data1'
-    //         //or hide: ['data1', 'data2']
-    //     },
-    //     color: {
-    //         pattern: ['#eceff1', '#24d2b5', '#6772e5', '#20aee3']
-    //     }
-    // });
-    // // ==============================================================
-    // // Our Income
-    // // ==============================================================
-    // var chart = c3.generate({
-    //     bindto: '#income',
-    //     data: {
-    //         columns: [
-    //             ['Growth Income', 100, 200, 100, 300],
-    //             ['Net Income', 130, 100, 140, 200]
-    //         ],
-    //         type: 'bar'
-    //     },
-    //     bar: {
-    //         space: 0.2,
-    //         // or
-    //         width: 15 // this makes bar width 100px
-    //     },
-    //     axis: {
-    //         y: {
-    //             tick: {
-    //                 count: 4,
-    //
-    //                 outer: false
-    //             }
-    //         }
-    //     },
-    //     legend: {
-    //         hide: true
-    //         //or hide: 'data1'
-    //         //or hide: ['data1', 'data2']
-    //     },
-    //     grid: {
-    //         x: {
-    //             show: false
-    //         },
-    //         y: {
-    //             show: true
-    //         }
-    //     },
-    //     size: {
-    //         height: 290
-    //     },
-    //     color: {
-    //         pattern: ['#24d2b5', '#20aee3']
-    //     }
-    // });
-    //
-    // // ==============================================================
-    // // Sales Different
-    // // ==============================================================
-    //
-    // var chart = c3.generate({
-    //     bindto: '#sales',
-    //     data: {
-    //         columns: [
-    //             ['One+', 50],
-    //             ['T', 60],
-    //             ['Samsung', 20],
-    //
-    //         ],
-    //
-    //         type: 'donut',
-    //         onclick: function (d, i) {
-    //             console.log("onclick", d, i);
-    //         },
-    //         onmouseover: function (d, i) {
-    //             console.log("onmouseover", d, i);
-    //         },
-    //         onmouseout: function (d, i) {
-    //             console.log("onmouseout", d, i);
-    //         }
-    //     },
-    //     donut: {
-    //         label: {
-    //             show: false
-    //         },
-    //         title: "",
-    //         width: 18,
-    //
-    //     },
-    //     size: {
-    //         height: 150
-    //     },
-    //     legend: {
-    //         hide: true
-    //         //or hide: 'data1'
-    //         //or hide: ['data1', 'data2']
-    //     },
-    //     color: {
-    //         pattern: ['#eceff1', '#24d2b5', '#6772e5', '#20aee3']
-    //     }
-    // });
-    // // ==============================================================
-    // // Sales Prediction
-    // // ==============================================================
-    //
-    // var chart = c3.generate({
-    //     bindto: '#prediction',
-    //     data: {
-    //         columns: [
-    //             ['data', 91.4]
-    //         ],
-    //         type: 'gauge',
-    //         onclick: function (d, i) {
-    //             console.log("onclick", d, i);
-    //         },
-    //         onmouseover: function (d, i) {
-    //             console.log("onmouseover", d, i);
-    //         },
-    //         onmouseout: function (d, i) {
-    //             console.log("onmouseout", d, i);
-    //         }
-    //     },
-    //
-    //     color: {
-    //         pattern: ['#ff9041', '#20aee3', '#24d2b5', '#6772e5'], // the three color levels for the percentage values.
-    //         threshold: {
-    //             //            unit: 'value', // percentage is default
-    //             //            max: 200, // 100 is default
-    //             values: [30, 60, 90, 100]
-    //         }
-    //     },
-    //     gauge: {
-    //         width: 22,
-    //     },
-    //     size: {
-    //         height: 120,
-    //         width: 150
-    //     }
-    // });
-    // setTimeout(function () {
-    //     chart.load({
-    //         columns: [
-    //             ['data', 10]
-    //         ]
-    //     });
-    // }, 1000);
-    //
-    // setTimeout(function () {
-    //     chart.load({
-    //         columns: [
-    //             ['data', 50]
-    //         ]
-    //     });
-    // }, 2000);
-    //
-    // setTimeout(function () {
-    //     chart.load({
-    //         columns: [
-    //             ['data', 70]
-    //         ]
-    //     });
-    // }, 3000);
-
-    // ==============================================================
-    // Sales chart
-    // ==============================================================
-    // Morris.Area({
-    //     element: 'sales-chart',
-    //     data: [{
-    //         period: '2011',
-    //         Sales: 50,
-    //         Earning: 80,
-    //         Marketing: 20
-    //     }, {
-    //         period: '2012',
-    //         Sales: 130,
-    //         Earning: 100,
-    //         Marketing: 80
-    //     }, {
-    //         period: '2013',
-    //         Sales: 80,
-    //         Earning: 60,
-    //         Marketing: 70
-    //     }, {
-    //         period: '2014',
-    //         Sales: 70,
-    //         Earning: 200,
-    //         Marketing: 140
-    //     }, {
-    //         period: '2015',
-    //         Sales: 180,
-    //         Earning: 150,
-    //         Marketing: 140
-    //     }, {
-    //         period: '2016',
-    //         Sales: 105,
-    //         Earning: 100,
-    //         Marketing: 80
-    //     },
-    //         {
-    //             period: '2017',
-    //             Sales: 250,
-    //             Earning: 150,
-    //             Marketing: 200
-    //         }
-    //     ],
-    //     xkey: 'period',
-    //     ykeys: ['Sales', 'Earning', 'Marketing'],
-    //     labels: ['Site A', 'Site B', 'Site C'],
-    //     pointSize: 0,
-    //     fillOpacity: 0,
-    //     pointStrokeColors: ['#20aee3', '#24d2b5', '#6772e5'],
-    //     behaveLikeLine: true,
-    //     gridLineColor: '#e0e0e0',
-    //     lineWidth: 3,
-    //     hideHover: 'auto',
-    //     lineColors: ['#20aee3', '#24d2b5', '#6772e5'],
-    //     resize: true
-    //
-    // });
-
-
-});
 
 function create_chart_section_count_chats(id, data) {
     let data_list = JSON.parse(data)
@@ -320,6 +61,15 @@ function create_chart_section_rate_chats(id, data) {
     let ctx = document.getElementById(`section-chart-rate-chats-${id}`).getContext('2d')
     let labels = []
     let data_num = []
+    let bg_color_rates = []
+    let dict_bg_color_rates = {
+        0: 'rgb(221,221,221)',
+        1: 'rgb(255,85,108)',
+        2: 'rgb(255,202,10)',
+        3: 'rgb(186,255,76)',
+        4: 'rgb(107,255,169)',
+        5: 'rgb(24,234,195)',
+    }
     for (let data of data_list) {
         if (data.rate_chat == 0) {
             labels.push(`بدون امتیاز `)
@@ -327,20 +77,14 @@ function create_chart_section_rate_chats(id, data) {
             labels.push(` امتیاز ${data.rate_chat} `)
         }
         data_num.push(data.count_rate)
+        bg_color_rates.push(dict_bg_color_rates[data.rate_chat])
     }
     let data_conf = {
         labels: labels,
         datasets: [{
             label: 'امتیاز گفت و گو ها',
             data: data_num,
-            backgroundColor: [
-                'rgb(221,221,221)',
-                'rgb(255,85,108)',
-                'rgb(255,202,10)',
-                'rgb(186,255,76)',
-                'rgb(107,255,169)',
-                'rgb(24,234,195)',
-            ],
+            backgroundColor: bg_color_rates,
         }]
     };
     let config = {

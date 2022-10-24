1f2fe80457dea03547159be98c8133a02b68be79
FZl47 <wwwfazel88@gmail.com>
2022-10-24 21:04:38 +0330

    remove useless code

--- SupChat/core/auth/view.py
+++ SupChat/core/auth/view.py
@@ -4,6 +4,5 @@ from SupChat.core import tools
 
 def create_user(request, phone_or_email):
     ip = tools.Get_IP(request)
-    print(ip)
     user = User.objects.create(phone_or_email=phone_or_email, ip=ip)
     return user

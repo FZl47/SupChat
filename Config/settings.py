eae57099ea87227ce267d6afe4c0f101205fbee3
FZl47 <wwwfazel88@gmail.com>
2023-02-01 00:05:14 +0330

    Add filter tags supchat in settings

--- Config/settings.py
+++ Config/settings.py
@@ -63,7 +63,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ]
+            ],
+            'libraries':{
+                'supchat_filter':'SupChat.templatetags.filter'
+            }
         },
     },
 ]

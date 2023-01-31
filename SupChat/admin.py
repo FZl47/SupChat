34d6a612873f8d4edb36212ba7ee9a3b4ae9ebb3
FZl47 <wwwfazel88@gmail.com>
2023-02-01 00:06:09 +0330

    remove some models from admin panel

--- SupChat/admin.py
+++ SupChat/admin.py
@@ -1,20 +1,13 @@
 from django.contrib import admin
 from SupChat.models import *
 
-
 admin.site.register(SupChat)
 admin.site.register(SupChatStyle)
 admin.site.register(SupChatConfig)
 admin.site.register(Admin)
 admin.site.register(User)
 admin.site.register(Section)
-admin.site.register(SuggestedMessage)
-
-admin.site.register(Message)
-admin.site.register(TextMessage)
-admin.site.register(AudioMessage)
 admin.site.register(SystemMessage)
-admin.site.register(SearchHistoryAdmin)
 admin.site.register(BlackList)
 
 @admin.register(ChatGroup)
